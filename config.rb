da80ed06fef219a8a50b61404c5098b9ace2c3ce
Kazuhiro Sera <seratch@gmail.com>
2014-07-23 21:29:42 +0900

    1.2.1

--- config.rb
+++ config.rb
@@ -57,9 +57,9 @@ set :markdown, fenced_code_blocks: true, smartypants: true, autolink: true, with
 I18n.enforce_available_locales = false
 
 # Latest Skinny Framework version
-@skinny_version = "1.2.0"
+@skinny_version = "1.2.1"
 set :skinny_version, @skinny_version
-@skinny_blank_app_version = "1.2.0-2"
+@skinny_blank_app_version = @skinny_version
 set :skinny_blank_app_version, @skinny_blank_app_version
 
 # Build-specific configuration
