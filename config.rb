27c4b8f020b40d985b04ce046c5d9e248a24fef5
Kazuhiro Sera <seratch@gmail.com>
2015-03-01 11:43:29 +0900

    1.3.14

--- config.rb
+++ config.rb
@@ -57,9 +57,9 @@ set :markdown, fenced_code_blocks: true, smartypants: true, autolink: true, with
 I18n.enforce_available_locales = false
 
 # Latest Skinny Framework version
-@skinny_version = "1.3.13"
+@skinny_version = "1.3.14"
 set :skinny_version, @skinny_version
-@scalikejdbc_version = "2.2.3"
+@scalikejdbc_version = "2.2.4"
 set :scalikejdbc_version, @scalikejdbc_version
 @skinny_blank_app_version = @skinny_version
 set :skinny_blank_app_version, @skinny_blank_app_version
