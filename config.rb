3cb5d62a2b61cc3777ffc209c7660a31667f6cb2
Kazuhiro <seratch@gmail.com>
2016-03-26 15:02:41 +0900

    skinny micro 1.0.4

--- config.rb
+++ config.rb
@@ -62,7 +62,7 @@ set :skinny1_version, @skinny1_version
 @skinny1_blank_app_version = @skinny1_version
 set :skinny1_blank_app_version, @skinny1_blank_app_version
 
-set :skinny_micro_version, "1.0.3"
+set :skinny_micro_version, "1.0.4"
 @skinny_version = "2.0.7"
 set :skinny_version, @skinny_version
 @skinny_blank_app_version = @skinny_version
