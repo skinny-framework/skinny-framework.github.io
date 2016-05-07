c1f2675a709d0a112c10354f98f470d1984037b8
Kaz Sera <seratch@gmail.com>
2016-05-08 01:34:50 +0900

    skinny 2.1.0

--- config.rb
+++ config.rb
@@ -62,13 +62,13 @@ set :skinny1_version, @skinny1_version
 @skinny1_blank_app_version = @skinny1_version
 set :skinny1_blank_app_version, @skinny1_blank_app_version
 
-set :skinny_micro_version, "1.0.4"
-@skinny_version = "2.0.9"
+set :skinny_micro_version, "1.0.5"
+@skinny_version = "2.1.0"
 set :skinny_version, @skinny_version
 @skinny_blank_app_version = @skinny_version
 set :skinny_blank_app_version, @skinny_blank_app_version
 
-@scalikejdbc_version = "2.3.5"
+@scalikejdbc_version = "2.4.0"
 set :scalikejdbc_version, @scalikejdbc_version
 
 # Build-specific configuration
