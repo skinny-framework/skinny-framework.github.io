d5c948572260b4bcb45a5ef889d42f56570dfeb4
Kaz Sera <seratch@gmail.com>
2016-05-21 18:54:31 +0900

    version 2.1.1

--- config.rb
+++ config.rb
@@ -63,7 +63,7 @@ set :skinny1_version, @skinny1_version
 set :skinny1_blank_app_version, @skinny1_blank_app_version
 
 set :skinny_micro_version, "1.0.5"
-@skinny_version = "2.1.0"
+@skinny_version = "2.1.1"
 set :skinny_version, @skinny_version
 @skinny_blank_app_version = @skinny_version
 set :skinny_blank_app_version, @skinny_blank_app_version
