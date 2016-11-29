7a0c1ef9c8d8be204c2a2f39294f57e7a4225b32
Kazuhiro Sera <seratch@gmail.com>
2016-11-29 20:12:10 +0900

    version 2.3.1

--- config.rb
+++ config.rb
@@ -65,7 +65,7 @@ set :skinny1_version, @skinny1_version
 set :skinny1_blank_app_version, @skinny1_blank_app_version
 
 set :skinny_micro_version, "1.2.0"
-@skinny_version = "2.3.0"
+@skinny_version = "2.3.1"
 set :skinny_version, @skinny_version
 @skinny_blank_app_version = @skinny_version
 set :skinny_blank_app_version, @skinny_blank_app_version
