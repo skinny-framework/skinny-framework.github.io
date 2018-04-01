dfea29590a8c6420851ef5d27072230d0bb1177c
Kazuhiro Sera <kazuhiro.sera@smartnews.com>
2018-04-01 14:54:47 +0900

    version 2.6.0

--- config.rb
+++ config.rb
@@ -64,13 +64,13 @@ set :skinny1_version, @skinny1_version
 @skinny1_blank_app_version = @skinny1_version
 set :skinny1_blank_app_version, @skinny1_blank_app_version
 
-set :skinny_micro_version, "1.2.8"
-@skinny_version = "2.5.2"
+set :skinny_micro_version, "1.3.0"
+@skinny_version = "2.6.0"
 set :skinny_version, @skinny_version
 @skinny_blank_app_version = @skinny_version
 set :skinny_blank_app_version, @skinny_blank_app_version
 
-@scalikejdbc_version = "3.1.0"
+@scalikejdbc_version = "3.2.3"
 set :scalikejdbc_version, @scalikejdbc_version
 
 # Build-specific configuration
@@ -96,4 +96,3 @@ activate :deploy do |deploy|
   deploy.method = :git
   deploy.branch = 'master'
 end
-
